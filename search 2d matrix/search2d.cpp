975753a26cbd57a6db2ea7d5149ad9b6e389cef9
Anshuwadhwani08 <anshuwadhwani08@gmail.com>
2024-01-19 22:41:07 +0530

    18-19 Jan

---
+++ search 2d matrix/search2d.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i=0;
+        int j=(n-1);
+        bool res = false;
+        while(i<m && j>=0){
+            if(matrix[i][j]>target){
+                j--;
+            }
+            else if(matrix[i][j]==target){
+              res = true;
+              break;  
+            }
+            else if(matrix[i][j]<target){
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+
+        }
+        return res;
+    }
+};
